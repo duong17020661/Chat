6fa5374654fad33ad21a3a867901e9dd11fbe209
DUONG TRAN HAI <thduong@rd.local>
2020-07-02 15:08:35 +0700

    chinh sua img

--- src/app/ui/chat/main-chat/main-chat.component.ts
+++ src/app/ui/chat/main-chat/main-chat.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { IMessages } from '../../../../models/messages';
 
 class FileSpinnet {
-  constructor(public src: string, public file: File) {}
+  constructor(public src: string, public file: File) { }
 }
 
 
@@ -17,6 +17,9 @@ export class MainChatComponent implements OnInit {
 
   public userID;
   public messages = [];
+  modal: any;
+  modalImg: any;
+  span: any;
 
   constructor(private _chatservice: MessagesService, private route: ActivatedRoute) {
     route.params.subscribe(val => {
@@ -27,6 +30,7 @@ export class MainChatComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.modalImg = document.getElementById("img");
     this._chatservice.getMessages().subscribe(data => this.messages = data);
     let id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.userID = id;
@@ -40,22 +44,22 @@ export class MainChatComponent implements OnInit {
     }
   }
 
-  @ViewChild('scrollframe', {static: false}) scrollFrame: ElementRef;
+  @ViewChild('scrollframe', { static: false }) scrollFrame: ElementRef;
   @ViewChildren('item') itemElements: QueryList<any>;
-  
+
   private scrollContainer: any;
   private items = [];
 
   ngAfterViewInit() {
-    this.scrollContainer = this.scrollFrame.nativeElement;  
-    this.itemElements.changes.subscribe(_ => this.onItemElementsChanged());    
+    this.scrollContainer = this.scrollFrame.nativeElement;
+    this.itemElements.changes.subscribe(_ => this.onItemElementsChanged());
 
     // Add a new item every 2 seconds
     setInterval(() => {
       this.items.push({});
     }, 2000);
   }
-  
+
   private onItemElementsChanged(): void {
     this.scrollToBottom();
   }
@@ -75,12 +79,12 @@ export class MainChatComponent implements OnInit {
     const reader = new FileReader();
     reader.addEventListener('load', (event: any) => {
       this.selectedFile = new FileSpinnet(event.target.result, file);
-      let message: IMessages = { id: this.messages.length + 1, message: this.selectedFile.src , senderID: 0, receiverID: this.userID, time: Date(), type: "image" };
+      let message: IMessages = { id: this.messages.length + 1, message: this.selectedFile.src, senderID: 0, receiverID: this.userID, time: Date(), type: "image" };
       this.messages.push(message)
     });
     reader.readAsDataURL(file);
-    
-    
+
+
   }
 
   processFile(fileInput: any) {
@@ -88,16 +92,21 @@ export class MainChatComponent implements OnInit {
     const reader = new FileReader();
     reader.addEventListener('load', (event: any) => {
       this.selectedFile = new FileSpinnet(event.target.result, file);
-      let message: IMessages = { id: this.messages.length + 1, message: this.selectedFile.file.name , senderID: 0, receiverID: this.userID, time: Date(), type: "file", url: this.selectedFile.src };
+      let message: IMessages = { id: this.messages.length + 1, message: this.selectedFile.file.name, senderID: 0, receiverID: this.userID, time: Date(), type: "file", url: this.selectedFile.src };
       this.messages.push(message)
       console.log(this.selectedFile.src)
     });
     reader.readAsDataURL(file);
   }
 
-  openFile(url: string){
+  openFile(url: string) {
     window.open(url, "");
   }
 
+  ModalImage(src: string){
+    this.modalImg.src = src;
+    this.modalImg.style.width = "auto";
+    this.modalImg.style.height = "auto";
+  }
 
 }
