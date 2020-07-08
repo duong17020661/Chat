15ade2a8d003aa3b074c24e0071d940c2b028c7a
DUONG TRAN HAI <thduong@rd.local>
2020-07-08 11:50:37 +0700

    Thêm comment 2

--- src/app/ui/chat/info-chat/info-chat.component.ts
+++ src/app/ui/chat/info-chat/info-chat.component.ts
@@ -12,13 +12,14 @@ import { DatatransferService } from 'src/app/services/datatransfer.service';
   styleUrls: ['./info-chat.component.scss']
 })
 export class InfoChatComponent implements OnInit {
-  public images = []
-  public messages = []
-  public file = []
-  public users = [];
-  public usersID;
-  modalImg: any;
+  public images = [] // List dữ liệu về ảnh
+  public messages = [] // List dữ liệu về tin nhắn
+  public file = [] // List dữ liệu về dile
+  public users = []; // List dữ liệu về người dùng
+  public usersID; // ID người dùng đang trỏ đến
+  modalImg: any; // Image modal
   constructor(private route: ActivatedRoute, private _datatransfer: DatatransferService, private _chatservice: MessagesService, private _userservice: UsersService) {
+    // Tạo lại các đối tượng khi có thay đổi
     route.params.subscribe(val => {
     this._userservice.getUsers().subscribe(data => this.users = data);
     this._chatservice.getMessages().subscribe(data => this.images = data);
@@ -29,44 +30,44 @@ export class InfoChatComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.modalImg = document.getElementById("img");
-    this._userservice.getUsers().subscribe(data => this.users = data);
-    this._chatservice.getMessages().subscribe(data => this.messages = data);
+    this.modalImg = document.getElementById("img"); // Lấy phần tử Image modal
+    this._userservice.getUsers().subscribe(data => this.users = data); // Lấy dữ liệu người dùng
+    this._chatservice.getMessages().subscribe(data => this.messages = data); // Lấy dữ liệu tin nhắn
+    // Lấy ID theo url
     let id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.getUserID()
-    console.log(this.usersID)
+    // Theo dõi sự thay đổi tin nhắn
     this._datatransfer.messages$.subscribe(value =>
       {
         if(value){
           this.messages.push(value)
         }
-          // do something
       });
   }
-
+  // Lọc dữ liệu tin nhắn ảnh
   getImages(){
     return this.messages.filter(mess => (mess.type === 'image') && ((mess.senderID === this.usersID) || (mess.receiverID === this.usersID)))
   }
+  // Lọc dữ liệu tin nhắn file
   getFiles(){
     return this.messages.filter(mess => (mess.type === 'file') && ((mess.senderID === this.usersID) || (mess.receiverID === this.usersID)))
   }
-
+  // Theo dõi sự thay đổi và lấy ID
   getUserID(){
     this._datatransfer.userId.subscribe(data => {
       this.usersID = data;
     })
   }
-
+  // Hiển thị ảnh và file theo dropdown
   isShowFile = false;
   isShowImg = false;
-  
   toggleDisplayFile() {
     this.isShowFile = !this.isShowFile;
   }
   toggleDisplayImg() {
     this.isShowImg = !this.isShowImg;
   }
-
+  // Thêm các thuộc tính cho modal image
   ModalImage(src: string){
     this.modalImg.src = src;
     this.modalImg.style.width = "auto";
